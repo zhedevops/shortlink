b3fcc7a9bc775551a5e0b95fb90b76c691e4dc72
zhedevops <iwstat@yandex.ru>
2026-03-02 23:47:30 +0300

    Инкремент 15. Каналы. Правки после ревью 4.

--- migrations/20260223071818_create_table_users.sql
+++ migrations/20260223071818_create_table_users.sql
@@ -5,13 +5,13 @@ CREATE TABLE IF NOT EXISTS users (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
-ALTER TABLE shortys ADD COLUMN user_id INT NOT NULL, ADD CONSTRAINT fk_user_id_shortys FOREIGN KEY (user_id) REFERENCES users (id);
+ALTER TABLE shortys ADD COLUMN user_id INT NOT NULL;-- todo, ADD CONSTRAINT fk_user_id_shortys FOREIGN KEY (user_id) REFERENCES users (id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 SELECT 'down SQL query';
-ALTER TABLE shortys DROP CONSTRAINT fk_user_id_shortys;
+--todo ALTER TABLE shortys DROP CONSTRAINT fk_user_id_shortys;
 ALTER TABLE shortys DROP COLUMN user_id;
 DROP TABLE IF EXISTS users CASCADE;
 -- +goose StatementEnd
